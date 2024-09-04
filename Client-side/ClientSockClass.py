c7b9c3182c83f5e16996214b92e7ea3d1e205b28
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-09-04 16:45:42 -0300

    feat: create cliente socket class

---
+++ Client-side/ClientSockClass.py
@@ -0,0 +1,18 @@
+import socket
+
+
+
+class ClientSocket():
+    def __init__(self, ip):
+        self.ip = ip
+        self.port = 8000
+        self.addr = (self.ip, self.port)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.client_socket.connect(self.addr)
+
+
+
+
+
