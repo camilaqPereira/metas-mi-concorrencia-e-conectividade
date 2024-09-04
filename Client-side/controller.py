d98e21e48992582a357d401150107561a92515fc
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-09-04 18:50:14 -0300

    feat: complete function with blank body

--- Client-side/controller.py
+++ Client-side/controller.py
@@ -1,25 +1,39 @@
-import socket
-import json
 from ClientSockClass import *
 
 ip = ""
 
-client = ClientSocket(ip)
+client = ClientSocket()
+
 
 def send_request(request):
-    size_transfer = len("request")
+    response = {}
+    size_transfer = len(request)
     client.client_socket.send(f'{size_transfer}'.encode('utf-8'))
     client.client_socket.send(request)
     size_transfer = client.client_socket.recv(1024)
-    response = client.client_socket.recv(int(size_transfer.decode('utf-8')))
+    server_response = client.client_socket.recv(int(size_transfer.decode('utf-8')))
+
+    if int(size_transfer.decode('utf-8')) == -1:
+        response = {'status': False}
+    else:
+        response = {'status': True, 'data': server_response}
 
     return response
 
+
 def buying(routes):
-    #envia a solicitação de compra
+    request = f"buy|{routes}"
+    response = send_request(request)
+
+    return response
+
 
 def conect(ip):
-    client.connect()
+    client.connect(ip)
+
 
 def search_routes(match, destination):
-    #envia a solicitação de trechos
+    request = f"routes|{match}|{destination}"
+    response = send_request(request)
+
+    return response
