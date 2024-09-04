51ce1730887e83381fb7f7f72d331bbcfeb88d1b
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-09-04 18:50:54 -0300

    refactor: refactoring function for connect

--- Client-side/ClientSockClass.py
+++ Client-side/ClientSockClass.py
@@ -3,13 +3,15 @@ import socket
 
 
 class ClientSocket():
-    def __init__(self, ip):
-        self.ip = ip
+    def __init__(self):
+        self.ip = ""
         self.port = 8000
         self.addr = (self.ip, self.port)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
+    def connect(self, ip):
+        self.ip = ip
+        self.addr = (self.ip, self.port)
         self.client_socket.connect(self.addr)
 
 
