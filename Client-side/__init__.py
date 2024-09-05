08001f03bf6eeecd59da538efde9905e50f2933b
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-09-04 21:37:00 -0300

    feat: client file for a new code struct

--- Client-side/__init__.py
+++ Client-side/__init__.py
@@ -1,16 +1,36 @@
 import socket
+from re import match
 
+import controller
 
 ip = '127.0.0.1'
-port = 8000
-addr = ((ip,port))
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(addr)
 
-mensagem = input('digite sua mensagem: ')
-client_socket.send(mensagem.encode('utf-8'))
-print ('mensagem enviada' )
-print('aguardando resposta')
-response = client_socket.recv(1024).decode('utf-8')
-print('server response: ' + response)
-client_socket.close()
+controller.conect(ip)
+
+inicio = input("informe a sua origem: ")
+destino = input("informe seu destino: ")
+
+routes = controller.search_routes(match=inicio, destination=destino)
+
+if routes['status']:
+    i = 0
+    for rota in routes['data'].keys():
+        print(f"opcao {i}: {rota}")
+
+    opc = input("digite o numero da rota desejada: ")
+
+    response = controller.buying(opc)
+
+    if response['status']:
+        if response['status']['success']:
+            print("compra bem sucedida")
+        else:
+            print("compra nao realizada")
+
+elif routes['status'] == 0 and routes['raise']  == 0:
+    print("dados para a rota nao encontrados")
+
+else:
+    print("houve uma excecao")
+
+
