457270f90509cfa1d232e8599d13d37079d89454
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-12 19:47:17 -0300

    refactor: setting timestamp inside constructor

--- Server/ResponseClass.py
+++ Server/ResponseClass.py
@@ -2,17 +2,15 @@ import json
 import datetime
 
 class Response:
-    def __init__(self, timestamp = datetime.datetime.now(), status = 0, data = None, rs_type = ''):
-        self.timestamp = timestamp
+    def __init__(self, status = 0, data = None, rs_type = ''):
+        self.timestamp = datetime.datetime.now()
         self.status = status
         self.data = data
         self.rs_type = rs_type
 
     def to_json(self):
         response = {'type':self.rs_type, 'timestamp':self.timestamp, 'status':self.status, 'data':self.data}
-
         json_str = json.dumps(response)
-
         return json_str
 
     def from_json(self, json_str: str):
