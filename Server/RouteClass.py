da74477f58ed92c789236958096d14f146c46acc
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-13 17:01:38 -0300

    feat: adding string conversion to routeclass

--- Server/RouteClass.py
+++ Server/RouteClass.py
@@ -1,7 +1,17 @@
 class Route:
 
-    def __init__(self):
-        self.match = ''
-        self.destination = ''
-        self.sits = 0
-        self.id = None
+    def __init__(self, match, destination, id, sits=10):
+        self.match = match
+        self.destination = destination
+        self.sits = sits
+        self.id = id
+
+    def to_string(self):
+        return {'match': self.match, 'destination': self.destination, 'sits': self.sits, 'id': self.id}
+    
+    def from_string(self, data):
+        self.match = data['match']
+        self.destination = data['destination']
+        self.sits = data['sits']
+        self.id = data['id']
+
