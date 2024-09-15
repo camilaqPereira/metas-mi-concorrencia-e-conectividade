c8ad44f978e998b5941d4d4b2c986b53a397f900
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-15 02:02:21 -0300

    refactor: removing unused methods

--- Server/RouteClass.py
+++ Server/RouteClass.py
@@ -6,21 +6,6 @@ class Route:
         self.sits = sits
         self.id = id
 
-    def get_route(self):
-        return (self.id, self.match, self.destination)
-    
-    def get_sits(self):
-        return self.sits
-    
-    def set_sits(self, value):
-        self.sits = value
-
-    def dec_sits(self):
-        self.sits -= 1
-
-    def inc_sits(self):
-        self.sits += 1
-    
     def to_string(self):
         return {'match': self.match, 'destination': self.destination, 'sits': self.sits, 'id': self.id}
     
