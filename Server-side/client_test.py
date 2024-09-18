3986a4cb623c6cb7280d5e42a9a2db00b6933df1
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-18 01:08:20 -0300

    Update client_test.py

--- Server-side/client_test.py
+++ Server-side/client_test.py
@@ -1,25 +1,28 @@
 import socket
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 
-IP = socket.gethostbyname(socket.gethostname())
-HEADER = 64
-PORT = 8000
-ADDR = (IP,PORT)
-FORMAT = "utf-8"
+from Client.RequestsClass import Request
+from Server.utils import *
+from Server.ResponseClass import Response
+from Client.ClientSockClass import ClientSocket
 
+sock = ClientSocket(socket.gethostbyname(socket.gethostname()))
+sock.connect()
+request = Request(rq_type=METHOD_BUY, rq_data=[('A','C'), ('C', 'B')], client_token="ee250f4a9e41b93bb1e2627df36c796cee1678caf988d286a7181890e3ac7388")
+rq_json = request.to_json()
 
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(ADDR)
-
-msg = input('digite sua mensagem: ')
-msg_len = len(msg)
+msg_len = len(rq_json)
 send_len = str(msg_len).encode(FORMAT)
-send_len += b' ' * (HEADER - len(send_len))
-client_socket.send(send_len)
-client_socket.send(msg.encode('utf-8'))
-msg_len = client_socket.recv(64).decode(FORMAT)
-msg = client_socket.recv(int(msg_len)).decode(FORMAT)
+send_len += b' ' * (MAX_PKT_SIZE - len(send_len))
+sock.client_socket.send(send_len)
+sock.client_socket.send(rq_json.encode(FORMAT))
+msg_len =sock.client_socket.recv(MAX_PKT_SIZE).decode(FORMAT)
+msg = sock.client_socket.recv(int(msg_len)).decode(FORMAT)
 
-client_socket.close()
+sock.end()
 
-print(f"Msg received: {msg}")
+
+print(msg)
