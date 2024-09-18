eee2cc156a7868a463d927cbdef2f079810352d8
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-18 01:07:32 -0300

    feat: add path file constants

--- Server/utils.py
+++ Server/utils.py
@@ -1,10 +1,15 @@
 from socket import gethostname, gethostbyname
+import os
 
 FORMAT = "utf-8" 
 MAX_PKT_SIZE = 64 #tamanho fixo do primeiro pacote
 DEFAULT_PORT = 8000
 HOST = gethostbyname(gethostname())
 
+PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+USERS_FILE_PATH = PARENT_DIR+'/DB/users.json'
+TICKETS_FILE_PATH = PARENT_DIR+'/DB/tickets.json'
+GRAPH_FILE_PATH = PARENT_DIR+'/DB/graph.json'
 #Metodos
 METHOD_BUY = "!BUY"
 METHOD_GETROUTES = "!GETROUTES"
