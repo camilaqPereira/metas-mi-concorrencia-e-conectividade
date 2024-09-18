02aa6de92569975bb93afcea0a243652ef5132f2
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-18 01:08:10 -0300

    Adding database files

--- DB/create_db.py
+++ DB/create_db.py
@@ -1,10 +1,16 @@
 from json import dump, load
-from utils import adjacency_list, parse_to_dict
+from utils import *
 import os
 
+'''
 file_name = os.path.abspath(os.path.join(os.path.dirname(__file__), 'graph.json'))
 
 serialized_list = {str(key): value for key, value in adjacency_list.items()}
 with open(file_name, "w") as file:
     dump(serialized_list, file)
+'''
 
+serialized = {str(key): value.to_string() for key, value in flights.items()}
+
+with open(ROUTES_DATA_FILE_PATH, 'w') as file:
+    dump(serialized, file)
--- DB/routes.py
+++ DB/routes.py
@@ -4,9 +4,9 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
 from scipy.sparse.csgraph import yen
-from graph import *
-from utils import *
-from json import load
+from DB.graph import *
+from DB.utils import *
+from json import load, dump
 
 
 def init_graph():
@@ -15,12 +15,28 @@ def init_graph():
             routes_graph = Graph(adjacency_list=load(file, object_pairs_hook=parse_to_dict))
     except FileNotFoundError:
         routes_graph = Graph()
-    
     return routes_graph
 
+def upload_routes_data():
+    try:
+        with open(NODES_FILE_PATH, 'r') as nodes_file:
+            nodes = nodes_file.read()
+        nodes = list(nodes.split(","))
+    except FileNotFoundError:
+        nodes = []
+    
+    try:
+        with open(ROUTES_DATA_FILE_PATH, 'r') as flights_file:
+            data = load(flights_file)
+        flights = {}
+        for key, value in data.items():
+            flights[eval(key)] = Route(match=value['match'], destination=value['destination'], sits=value['sits'], id=value['id'])
+    except FileNotFoundError:
+        flights = {}
 
+    return nodes, flights
 
-def search_route(routes_graph:Graph, match:int, destination:int):
+def search_route(routes_graph:Graph, matches_and_destinations:list, flights:dict, match:int, destination:int):
     match_index = matches_and_destinations.index(match)
     destination_index = matches_and_destinations.index(destination)
 
