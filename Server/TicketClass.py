61ff6dbde465636ad1575641c863e5961fc78eb0
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-18 01:06:33 -0300

    feat: adding save method

--- Server/TicketClass.py
+++ Server/TicketClass.py
@@ -1,12 +1,32 @@
 import json
 from datetime import datetime
+import os
+import sys
 
+PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(PARENT_DIR)
+from Server.utils import TICKETS_FILE_PATH
 
 class Ticket:
-    def __init__(self, email='', timestamp = datetime.now(), routes=None):
+    def __init__(self, email='', routes=None):
         self.email = email
-        self.timestamp = timestamp
+        self.timestamp = datetime.now()
         self.routes = routes
+
+    def save(self):
+        data = {'timestamp': self.timestamp.strftime('%d/%m/%y %H:%M:%S'), 'routes': self.routes}
+        try:
+            with open(TICKETS_FILE_PATH, 'x+') as file:
+                json.dump({self.email: [data]}, file)
+        except FileExistsError:
+            with open(TICKETS_FILE_PATH, 'r+') as file:
+                all_tickets = json.load(file)
+                if self.email in all_tickets:
+                    all_tickets.get(self.email).append(data)
+                else:
+                    all_tickets[self.email] = [data]
+                file.seek(0)
+                json.dump(all_tickets, file)
 
     def from_json(self, json_str):
         values = json.loads(json_str)
