f94f99d2e73eb4bb23bfa8e8c0cc7300b0caaf61
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-18 01:07:15 -0300

    fix: data conflicts

--- Server/ResponseClass.py
+++ Server/ResponseClass.py
@@ -2,14 +2,14 @@ import json
 import datetime
 
 class Response:
-    def __init__(self, timestamp = datetime.datetime.now(), status = 0, data = None, rs_type = ''):
-        self.timestamp = timestamp
+    def __init__(self, status = 0, data = None, rs_type = ''):
+        self.timestamp = datetime.datetime.now()
         self.status = status
         self.data = data
         self.rs_type = rs_type
 
     def to_json(self):
-        response = {'type':self.rs_type, 'timestamp':self.timestamp.strftime('%d/%m/%y %H:%M:%S'), 'status':self.status, 'data':self.data}
+        response = {'type':self.rs_type, 'timestamp':self.timestamp.strftime('%d/%m/%Y %H:%M:%S'), 'status':self.status, 'data':self.data}
 
         json_str = json.dumps(response)
 
