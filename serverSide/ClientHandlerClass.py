c58345ee67a7e477cbc7a811da3e4ed62444ef2b
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-20 00:58:16 -0300

    feat: adding mutex for synchronization

--- serverSide/ClientHandlerClass.py
+++ serverSide/ClientHandlerClass.py
@@ -10,7 +10,6 @@ class ClientHandler:
     #Mutexes
     users_file_lock = Lock()
     tickets_file_lock = Lock()
-    graph_lock = Lock()
 
     def __init__(self, conn:socket = None, addr = None):  
         self.conn = conn
@@ -70,14 +69,11 @@ class ClientHandler:
         return True if (token in users.values()) else False
 
     def find_routes(self, server_data: ServerData, match:str, destination:str):
-        if (match not in server_data.matches_and_destinations) or (destination not in server_data.matches_and_destinations):
-            return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-        else:
             found_routes = server_data.search_route(match, destination)
-            if found_routes[0]:
-                return (ConstantsManagement.OK.value, found_routes, ConstantsManagement.ROUTE_TYPE.value)
-            else:
+            if (found_routes is None) or (not found_routes[0]):
                 return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
+            else:
+                return (ConstantsManagement.OK.value, found_routes, ConstantsManagement.ROUTE_TYPE.value)
 
     def buy_routes(self, server_data:ServerData, token, routes:list):
         routes_keys = []
@@ -85,26 +81,26 @@ class ClientHandler:
         for item in routes: #verificando validade dos voos 
             try:
                 flight_key = (server_data.matches_and_destinations.index(item[0]), server_data.matches_and_destinations.index(item[1]))
-                if not server_data.flights[flight_key].sits:
+                if not server_data.get_flight_sit(flight_key):
                     return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
                 routes_keys.append(flight_key)
             except ValueError:
                 return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
             
             for item in routes_keys:
-                server_data.flights[item].sits -= 1
-                if not server_data.flights[item]:
-                    server_data.set_graph_edge_weight(item[0], item[1], 9999)
-                    server_data.sparse_matrix[item[0], item[1], 9999]
+                server_data.dec_flight_sits(item)
             
             try:
                 with open(FilePathsManagement.ROUTES_DATA_FILE_PATH.value, 'w') as file:
                     dump(server_data.parse_flights_to_str(), file)
             except FileNotFoundError:
                 print("[SERVER] Could not update the graph file! File doesn't exist")
-        #unlock
+        
+
         ticket = Ticket(self.__get_email(token), routes)
-        ticket.save()
+        with ClientHandler.tickets_file_lock:
+            ticket.save()
+        
         return (ConstantsManagement.OK.value, ticket.to_json(), ConstantsManagement.TICKET_TYPE.value)
             
     def __get_email(self, token):
@@ -117,16 +113,13 @@ class ClientHandler:
         
     def get_tickets(self, token):
         try:
-            with open(FilePathsManagement.TICKETS_FILE_PATH.value, 'r') as file:
-                all_tickets:dict = load(file)   
+            with ClientHandler.tickets_file_lock:
+                with open(FilePathsManagement.TICKETS_FILE_PATH.value, 'r') as file:
+                    all_tickets:dict = load(file)   
             email = self.__get_email(token)
             users_tickets = all_tickets.get(email)
-        except FileNotFoundError:
-            users_tickets = None
-
-        if users_tickets:
             return (ConstantsManagement.OK.value, users_tickets, ConstantsManagement.TICKET_TYPE.value)
-        else:
+        except FileNotFoundError:
             return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
 
 
