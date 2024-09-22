c39a8de4b4730168594a6f0de5795f2b706805e0
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-09-22 01:40:48 -0300

    refactor: simpliflying methods
    - adding handling to custom exceptions
    - simplifying return values

--- Server_Docker/serverSide/ClientHandlerClass.py
+++ Server_Docker/serverSide/ClientHandlerClass.py
@@ -2,84 +2,86 @@ from hashlib import sha256
 from DB.utils import *
 from server.requests import *
 import socket
-from json import load, dump, dumps
+from serverSide.customExceptions import InvalidTokenException, ClientNotFoundException
 
 class ClientHandler:
 
-    def __init__(self, conn:socket = None, addr = None):  
+    def __init__(self, conn, addr):  
         self.conn = conn
         self.addr = addr
     
     def create_user(self, email:str):
         token = sha256(email.encode(ConstantsManagement.FORMAT.value)).hexdigest()
         created_status = UsersData.save_user(email, token)
-        if created_status:
-            return (ConstantsManagement.OK.value, token, ConstantsManagement.TOKEN_TYPE.value)
-        else:
-            return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-        
+        return token if created_status else None
 
     def get_token(self, email:str):
-        users:dict = UsersData.load_users()
-        token = users.get(email)
-        if token:
-            return (ConstantsManagement.OK.value, token, ConstantsManagement.TOKEN_TYPE.value)
-        else:
-            return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
+        try:
+
+            users:dict = UsersData.load_users()
+            token = users.get(email)
+            if token:
+                return token
+            else:
+                raise ClientNotFoundException('Client not found')
+        
+        except FileExistsError:
+           raise
+        except ClientNotFoundException:
+            print(f'[SERVER] {self.addr} Client not registered')
+            raise 
     
     def auth_token(self, token = None):
-        users:dict = UsersData.load_users()
-        if token and (token in users.values()):
+        try:
+            users:dict = UsersData.load_users()
+            if (not token) or (token not in users.values()):
+                raise InvalidTokenException('Token is not registered!')
             return True
-        else:
-            return False
+        except FileExistsError:
+           raise
+        except InvalidTokenException:
+            print(f'[SERVER] {self.addr} Invalid token')
+            raise
 
-    def find_routes(self, server_data: ServerData, match:str, destination:str):
-            found_routes = server_data.search_route(match, destination)
-            if (found_routes is None) or (not found_routes[0]):
-                return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
+
+    def buy_routes(self, server_data:ServerData, token:str, routes:list[tuple[str,str]]):
+        try:
+            email = self.__get_email(token)
+            if server_data.dec_all_routes(routes):
+                ticket = Ticket(email, routes)
+                ticket.save()
+                return ticket
             else:
-                return (ConstantsManagement.OK.value, found_routes, ConstantsManagement.ROUTE_TYPE.value)
-
-    def buy_routes(self, server_data:ServerData, token, routes:list):
-        routes_keys = []
-        for item in routes: #verificando validade dos voos 
-            try:
-                flight_key = (server_data.matches_and_destinations.index(item[0]), server_data.matches_and_destinations.index(item[1]))
-                if not server_data.get_flight_sit(flight_key):
-                    return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-                routes_keys.append(flight_key)
-            except ValueError:
-                return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-            except KeyError:
-                return (ConstantsManagement.OPERATION_FAILED.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-        for item in routes_keys:
-            server_data.dec_flight_sits(item)
+                return None
+        except FileNotFoundError:
+            raise
+        except ClientNotFoundException:
+            raise
         
-        server_data.save_graph()
-
-        ticket = Ticket(self.__get_email(token), routes)
-        ticket.save()
-        
-        return (ConstantsManagement.OK.value, ticket.to_json(), ConstantsManagement.TICKET_TYPE.value)
             
-    def __get_email(self, token):
-        users:dict = UsersData.load_users()
-        for user, client_token in users.items():
-            if client_token == token:
-                return user
-        return None
-        
-    def get_tickets(self, token):
-        email = self.__get_email(token)
-        all_tickets:dict = Ticket.load_tickets()
-        users_tickets = all_tickets.get(email)
+    def __get_email(self, token:str):
+        try:
+            users:dict[str,str] = UsersData.load_users()
+            for user, client_token in users.items():
+                if client_token == token:
+                    return user
+            
+            raise(ClientNotFoundException('Client not found'))
+        except FileNotFoundError:
+            raise
+        except ClientNotFoundException:
+            print(f'[SERVER] {self.addr} Client not found')
+            raise
 
-        if email and users_tickets:
-            return (ConstantsManagement.OK.value, users_tickets, ConstantsManagement.TICKET_TYPE.value)
         
-        return (ConstantsManagement.NOT_FOUND.value, None, ConstantsManagement.NO_DATA_TYPE.value)
-        
+    def get_tickets(self, token:str):
+        try:
+            email = self.__get_email(token)
+            all_tickets:dict = Ticket.load_tickets()
+            return all_tickets.get(email)
+
+        except (ClientNotFoundException, FileNotFoundError):
+            raise
 
     def receive_pkt(self):
         pkt = Request()
@@ -94,8 +96,12 @@ class ClientHandler:
             pkt = None
         return pkt
     
-    def send_pkt(self, return_values:tuple): #(status, data, data_type)
-        pkt = Response(return_values[0], return_values[1], return_values[2])
+    ##
+    #   @brief: Realiza o envio de pacotes do cliente
+    #
+    #   @raises: OSError caso ocorra uma falha na conexão
+    ##
+    def send_pkt(self, pkt:Response):
         pkt_json = pkt.to_json()
         try:
             pkt_len = str(len(pkt_json)).encode(ConstantsManagement.FORMAT.value)
@@ -107,3 +113,4 @@ class ClientHandler:
             print(f"[SERVER] Package transfer to {self.addr} failed! {str(err)}\n")
             status = False
         return status
+    
